47960d5d32d70910da5954df5cee4eeade87bf31
Jürgen Gorobez <university@gorobez.com>
2023-03-19 18:10:52 +0100

    importing long term Z signal and keep header with body separated in a class function

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import numpy as np
 import pandas as pd
-import io
+import logging
+import re
+
+class Nanonis_long_term_data_to_fft:
+    # Configure the logging level
+    logging.basicConfig(level=logging.DEBUG)  # Change this to logging.INFO to hide debug messages
+
+    # constant values
+
+    def __init__(self, fname, sep="\t"):
+        self.fname = fname
+        # self.data_clean = data_clean
+        self.start_read_from_here = "[DATA]"
+        self.start_read_form_index = None
+
+    def load_file(self):
+        with open(self.fname, 'r') as f:
+            for i, line in enumerate(f):
+                if self.start_read_from_here in line:
+                    self.start_read_form_index = i
+                    break
+
+        df_raw_header = pd.read_csv(self.fname, sep="\t", decimal=",", nrows=self.start_read_form_index + 1)
+        df_raw_body = pd.read_csv(self.fname, sep="\t", decimal=",", skiprows=range(1, self.start_read_form_index + 2))
+
+        logging.debug(f"Temporary argument for debugging:"
+                      f"\n raw_header:"
+                      f"\n{df_raw_header}"
+                      f"\n raw_body:"
+                      f"\n{df_raw_body}")
+        return df_raw_header, df_raw_body
 
 
-def load_file():
-    data = pd.read_csv("long_term_signal.dat")
+if __name__ == "__main__":
+    tfile = Nanonis_long_term_data_to_fft("6Hz_Z_signal_clean001.dat").load_file()
 
-
-    return data
-
-def slope():
-    data = load_file()
-    return data
-
-# var = np.fft()
-
-dt= slope()
-#if __name__ == "__main__":
-#    dt = slope()
-#    print("done")

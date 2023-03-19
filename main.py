0662c7c315390ac6ff5e8e5f1fc7369a0d815643
Jürgen Gorobez <university@gorobez.com>
2023-03-20 00:48:45 +0100

    import of steady standing Z signals (2D-array) and plot in 4 subplots

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Tuple
 
 import numpy as np
 import pandas as pd
@@ -10,104 +10,144 @@ from numpy import ndarray
 
 class NanonisLongTermDataToFFT:
     # Configure the logging level
-    logging.basicConfig(level=logging.INFO)  # Change this to logging.INFO or DEBUG to hide debug messages
+    logging.basicConfig(level=logging.DEBUG)  # Change this to logging.INFO or DEBUG to hide debug messages
 
     def __init__(self, fname: str):
-        self._end_of_header_start_data_index = None
         self.fname = fname
         self.separator = "\t"
         self.decimal = ","
         self.end_of_header_start_data = "[DATA]"
         self._raw_file = None
-        self._get_header = None
-        self.get_array = None
-        self.time = None
-        self.np_flatten_signal = None
 
     def get_file(self):
         self._raw_file = pd.read_csv(self.fname, sep=self.separator, decimal=self.decimal)
+        logging.debug(f"\n raw_pandas_file: \n"
+                      f"{self._raw_file}")
         return self._raw_file
 
     def end_of_header_start_data_index(self):
         with open(self.fname, 'r') as f:
             for i, line in enumerate(f):
                 if self.end_of_header_start_data in line:
-                    self._end_of_header_start_data_index = i
+                    end_of_header_start_data_index = i
                     break
-            return self._end_of_header_start_data_index
-    def get_header(self):
-        self.get_header = self._raw_file[:self.end_of_header_start_data_index]
-        logging.debug(f"\n pandas_dataframe_head: \n"
-                      f"{self.get_header}")
-        return self._get_header
+            logging.debug(f"\n end_of_header_start_data_index: \n"
+                          f"{end_of_header_start_data_index}")
+            return end_of_header_start_data_index
 
-    def get_array(self):
-        df_raw_body = self._raw_file[self.end_of_header_start_data_index + 2:]
-        self.get_array = df_raw_body.to_numpy().astype(float)
+    def get_header(self, index):
+        get_header = self._raw_file[:index]
+        logging.debug(f"\n pandas_dataframe_header: \n"
+                      f"{get_header}")
+        return get_header
+
+    def get_array(self, index):
+        raw_body = self._raw_file[index:]
+        get_array = raw_body.to_numpy().astype(float)
 
         logging.debug(f"\n raw_body: \n"
-                      f"{df_raw_body}")
-        return self.get_array
+                      f"{raw_body}")
+        return get_array
 
-    def linear_regression(self: np.ndarray[float]) -> tuple[ndarray, ndarray]:
-        # noinspection PyTupleAssignmentBalance
-        slope, intercept = np.polyfit(self.df_np_body[:, 0], self.df_np_body[:, 1], 1)  # x, y , 1 = linear fit
-        logging.debug(f"\n"
-                      f"slope: {slope}\n"
-                      f"intercept: {intercept}")
-        return slope, intercept
 
-    def fft_signal(self):
-        # Sampling Data
-        time = self.get_array[:, 0]
-        signal = self.get_array[:, 1]
+def lin_regression(array_2D):
+    slope, intercept = np.polyfit(array_2D[:, 0], array_2D[:, 1], 1)  # x, y , 1 = linear fit
+    logging.debug(f"\n"
+                  f"slope: {slope}\n"
+                  f"intercept: {intercept}")
+    return slope, intercept
 
-        # linear function y = m*x + b
-        linear_function = linear_regression[0] * self.time + linear_regression[1]
 
-        # subtracts slope from the signal -> preparing for FFT
-        self.np_flatten_signal = signal - linear_function
+def fft_signal(array_2D, slope, intercept):
+    # Sampling Data
+    time = array_2D[:, 0]
+    signal = array_2D[:, 1]
 
-        # Calculate the FFT
-        fft_signal = np.fft.fft(self.np_flatten_signal)
+    # linear function y = m*x + b
+    linear_function = slope * time + intercept
 
-        # Calculate the frequency values
-        num_samples = len(self.time)
-        sampling_interval = time[1] - time[0]
-        self.freq = np.fft.fftfreq(num_samples, sampling_interval)
+    # subtracts slope from the signal -> preparing for FFT
+    signal_minus_slope = signal - linear_function
 
-        # Calculate the amplitude
-        self.power = np.abs(fft_signal)
+    # Calculate the FFT
+    fft_signal = np.fft.fft(signal_minus_slope)
 
-        return freq, power
+    # Calculate the frequency values
+    num_samples = len(time)
+    sampling_interval = time[1] - time[0]
+    freqency = np.fft.fftfreq(num_samples, sampling_interval)
 
-    def plot_graph(self):
-        # Check if running in debug mode
-        if __debug__:
-            plt.plot(self.time, self.np_flatten_signal)
-            plt.xlabel('Time (s)')
-            plt.ylabel('Amplitude (nm)')
-            plt.title('???')
-            # plt.xlim(0, 50)
-            # plt.yticks(ticks=plt.yticks()[0], labels=plt.yticks()[0] * 10E9)  # multiply y-Axis by 10E9 -> now in nm
-            plt.grid()
-            plt.show()
-        else:
-            print("Not in debug mode")
+    # Calculate the amplitude
+    power = np.abs(fft_signal)
 
-        # Plot the frequency vs amplitude
-        plt.plot(self.fft_signal[0], self.fft_signal[1])
-        plt.xlabel('Frequency (Hz)')
-        plt.ylabel('Amplitude (nm)')
-        plt.title('FFT of a Stable Tip ')
-        # plt.xlim(0, 50)
-        plt.yticks(ticks=plt.yticks()[0], labels=plt.yticks()[0] * 10E9)  # multiply y-Axis by 10E9 -> now in nm
-        plt.grid()
+    # calculated frequency and amplitude (power), additionally: linear_function, signal_without_slope
+    # raw data: time, signal
+    return freqency, power, time, signal, linear_function, signal_minus_slope
+
+
+def plot_graph():
+    # Check if running in debug mode
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1)
+
+    if __debug__:
+        # Plot on the first subplot
+        ax4.plot(frequency, power)
+        ax4.set_title('Sine Function')
+        ax4.set_xlabel('Frequency (Hz)')
+        ax4.set_ylabel('Amplitude (nm)')
+        ax4.grid()
+
+        # Plot on the second subplot
+        ax1.plot(time, signal)
+        ax1.set_title('raw_signal')
+        ax1.set_xlabel('Time (s)')
+        ax1.set_ylabel('Amplitude (nm)')
+        ax1.grid()
+
+        # Plot on the second subplot
+        ax2.plot(time, linear_function)
+        ax2.set_title('fitted_linear_func')
+        ax2.set_xlabel('Time (s)')
+        ax2.set_ylabel('Amplitude (nm)')
+        ax2.grid()
+
+        # Plot on the second subplot
+        ax3.plot(time, signal_minus_slope)
+        ax3.set_title('sinal_without_slope')
+        ax3.set_xlabel('Time (s)')
+        ax3.set_ylabel('Amplitude (nm)')
+        ax3.grid()
+
         plt.show()
+
+
+
+    else:
+        print("Not in debug mode")
+
+    # # Plot the frequency vs amplitude
+    # plt.plot(frequency, power)
+    # plt.xlabel('Frequency (Hz)')
+    # plt.ylabel('Amplitude (nm)')
+    # plt.title('FFT of a Stable Tip ')
+    # # plt.xlim(0, 50)
+    # plt.yticks(ticks=plt.yticks()[0], labels=plt.yticks()[0] * 10E9)  # multiply y-Axis by 10E9 -> now in nm
+    # plt.grid()
+    # plt.show()
 
 
 if __name__ == "__main__":
     file = NanonisLongTermDataToFFT("6Hz_Z_signal_clean001.dat")
-    aa = file.get_file()
-   var = file._end_of_header_start_data_index()
+
+    whole_file = file.get_file()
+    header_index = file.end_of_header_start_data_index()
+    header = file.get_header(header_index)
+    data = file.get_array(header_index)
+    linear_regression = lin_regression(data)
+    frequency, power, time, signal, linear_function, signal_minus_slope = fft_signal(data, linear_regression[0],
+                                                                                     linear_regression[1])
+    plot_graph()
+
+    # header = file.get_header()
+    # var = print(type(file.end_of_header_start_data_index))
     # file.get_header()
